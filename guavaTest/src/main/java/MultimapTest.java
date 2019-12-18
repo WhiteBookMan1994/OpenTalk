b1aaa6d463d2aca2031747bacfb061d6084ccfa3
dingchenchen <>
2019-12-18 11:40:26 +0800

    feat:
    Multimap测试

---
+++ guavaTest/src/main/java/MultimapTest.java
@@ -0,0 +1,24 @@
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+/**
+ * @author dingchenchen
+ * @since 2019-12-16
+ */
+public class MultimapTest {
+
+    public static void main(String[] args) {
+        Multimap<Integer,Integer> multimap = ArrayListMultimap.create();
+        multimap.put(1,1);
+        multimap.put(1,2);
+        multimap.put(1,3);
+        multimap.put(2,2);
+        multimap.put(3,3);
+        multimap.put(null,null);
+        multimap.put(null,6);
+        System.out.println(multimap);
+        System.out.println(multimap.get(1));
+        //需要注意Multimap中不存在的key，会返回一个空List
+        System.out.println(multimap.get(12).isEmpty());
+    }
+}
