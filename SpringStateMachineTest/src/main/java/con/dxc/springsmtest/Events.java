737e7e018a31026b4630f0be933ea3e181e3fcba
dingchenchen <1936886538@qq.com>
2020-07-27 18:26:22 +0800

    add:Spring state machine 模块

---
+++ SpringStateMachineTest/src/main/java/con/dxc/springsmtest/Events.java
@@ -0,0 +1,9 @@
+package con.dxc.springsmtest;
+
+/**
+ * @author dingchenchen
+ * @since 2020/7/27
+ */
+public enum Events {
+    E1, E2
+}
---
+++ SpringStateMachineTest/src/main/java/con/dxc/springsmtest/States.java
@@ -0,0 +1,9 @@
+package con.dxc.springsmtest;
+
+/**
+ * @author dingchenchen
+ * @since 2020/7/27
+ */
+public enum States {
+    SI, S1, S2
+}
